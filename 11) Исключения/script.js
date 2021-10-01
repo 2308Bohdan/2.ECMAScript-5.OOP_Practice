7f4c4388f05ff5b82a09da34597aadda214a322f
2308Bohdan <63585465+2308Bohdan@users.noreply.github.com>
2021-10-01 11:50:39 +0300

    Add files via upload

---
+++ 11) Исключения/script.js
@@ -0,0 +1,37 @@
+"use strict";
+//Задание №1.
+
+function sumSliceArray(arr, first, second) {
+	if (typeof first !== "number" || typeof second !== "number"){
+		throw new Error("Введите корректно число");
+	} else if (first > arr.length || second > arr.length) {
+		throw new Error("Числo должнo быть меньше или равнo " + arr.length);
+	}
+	return arr[first - 1] + arr[second - 1];
+};
+
+var someArr = [1, 2, 3, 4, 5, 6, 7];
+
+try {
+	var res1 = sumSliceArray(someArr, 3, 1);
+	console.log(res1);
+} catch (error) {
+	console.log(error.name);
+	console.log(error.message);
+}
+
+try {
+	var res2 = sumSliceArray(someArr, "3", 1);
+	console.log(res2);
+} catch (error) {
+	console.log(error.name);
+	console.log(error.message);
+}
+
+try {
+	var res3 = sumSliceArray(someArr, 3, 9);
+	console.log(res3);
+} catch (error) {
+	console.log(error.name);
+	console.log(error.message);
+}
